eb16916b795fb192c021dc3a3ba745828c3ed2a5
zoo <designgip@naver.com>
2022-06-23 11:38:14 +0900

    first commit

---
+++ room.js
@@ -0,0 +1,36 @@
+export default[{ 
+    //배열요소로 상품명,사진,가격등을 배열{},{},{}
+    id : 0,
+    title: "비즈니스 디럭스 룸",
+    image: "../images/room01.png",
+    content: "깃털처럼 포근한 침구류를 갖춘 객실에서의 따스하고 아늑한 시간",
+    price: 475000
+    },
+    {
+    id : 1,
+    title: "Urban Island",
+    image: "../images/room02.png",
+    content: "따뜻한 봄날, 어번 아일랜드 온수풀에서의 여유로운 휴식",
+    price: 450000
+    },
+    {
+    id : 2,
+    title: "Little Soho Storytelling & Baking Class",
+    image: "../images/room03.png",
+    content: "6~9세 아이들을 대상으로 진행되는 영어 스토리텔링 & 베이킹 프로그램으로 동화 속 이야기를 케이크 또는 쿠키로 표현해 보는 특별한 시간",
+    price: 465000
+    },
+    {
+    id : 3,
+    title: "Deluxe Room",
+    image: "../images/room04.png",
+    content: "세계적인 수준의 침구류를 갖춘 여유로운 객실",
+    price: 550000
+    },
+    {
+    id : 4,
+    title: "Urban Island - Moonlight Swimming",
+    image: "../images/room05.png",
+    content: "은은한 달빛 가득한 어번 아일랜드에서 펼쳐지는 문라이트 시네마",
+    price: 680000
+    }];
